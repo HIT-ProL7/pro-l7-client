b024c72bb5f142af6a7a20b7a03cd2f65086547f
Khang Trần <148173085+kang0408@users.noreply.github.com>
2024-06-19 15:40:35 +0700

    feat: setup http (#4)
    * feat: setup http
    
    * feat: add .env to .gitignore and add .env.example file
    
    * feat: change code in axios.js
    
    * feat: Add instruction in readme file
    
    * feat: delete .env file in github

---
+++ src/api/axios.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const apiInst = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}`,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+apiInst.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem('prol7-vuejs:access-token');
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+apiInst.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status == 401) {
+      console.log('chuyen ve trang login');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export const api = apiInst;
