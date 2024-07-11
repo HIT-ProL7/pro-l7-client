2a6cfbf6639b89b1d5ed544e40fc0a69023d996f
thanhthao2602 <140616862+thanhthao2602@users.noreply.github.com>
2024-07-11 22:44:30 +0700

    feat: add class preview (#18)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,26 +1,67 @@
 <script setup lang="js">
-import { useRouter } from 'vue-router';
+import classrooms from '@/components/Classrooms/Classrooms.vue';
 
-const router = useRouter();
 const classes = [
-  { id: 'cl1', name: 'Python' },
-  { id: 'cl2', name: 'Photoshop' }
+  {
+    id: 'cl1',
+    name: 'Python',
+    leader: 'Phạm Đình Tiến',
+    startDate: '15/06/2024',
+    endDate: '15/08/2024',
+    logoType: 'python',
+  },
+  {
+    id: 'cl3',
+    name: 'Java',
+    leader: 'Phạm Đình Tiến',
+    startDate: '15/06/2024',
+    endDate: '15/08/2024',
+    logoType: 'java',
+  },
+  {
+    id: 'cl2',
+    name: 'Photoshop',
+    leader: 'Phạm Đình Tiến',
+    startDate: '15/06/2024',
+    endDate: '15/08/2024',
+    logoType: 'photoshop',
+  },
+  {
+    id: 'cl4',
+    name: 'Web',
+    leader: 'Phạm Đình Tiến',
+    startDate: '15/06/2024',
+    endDate: '15/08/2024',
+    logoType: 'web',
+  },
 ];
-
-function goToClassDetail(classId) {
-  router.push({ name: 'Class', params: { id: classId } });
-}
 </script>
 
 <template>
-  <div>
-    <div v-for="c in classes" :key="c.id" @click="goToClassDetail(c.id)">
-      <p>{{ c.name }}</p>
-    </div>
+  <div class="home">
+    <classrooms :classInfor="classes" />
   </div>
 </template>
 
 <style lang="scss" scoped>
+.home {
+  width: 100%;
+  overflow: hidden;
+  height: 100vh;
+  background-color: $body-color;
+  padding: 100px 50px 100px 50px;
+  @include tablet {
+    min-height: 100vh;
+  }
+  @include small-tablet {
+    min-height: 100vh;
+  }
+  @include mobile {
+    height: auto;
+    padding: 32px;
+  }
+}
+
 p {
   font-size: 24px;
 }
