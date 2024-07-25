12029edb011c0a1c74688906f2e26071ff4ef40e
thanhthao2602 <140616862+thanhthao2602@users.noreply.github.com>
2024-07-25 14:54:05 +0700

    fix_login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,6 +1,6 @@
 <script setup lang="js">
-import { reactive } from 'vue';
-
+import { reactive,ref } from 'vue';
+const forget = ref(false);
 const user = reactive({
   username: '',
   password: ''
@@ -58,63 +58,87 @@ function loginHandler() {
     return;
   }
 }
+const formData = reactive({
+  masv: '',
+});
+
+const errors = reactive({
+  masv: {
+    error: false,
+    errorMessage: '',
+  }
+});
+
+const rgxMasv = /^\d{10}$/;
+
+const masvValidate = () => {
+  if (formData.masv.trim() === '') {
+    errors.masv.error = true;
+    errors.masv.errorMessage = 'Vui lòng điền vào trường này';
+    return false;
+  } else if (!rgxMasv.test(formData.masv)) {
+    errors.masv.error = true;
+    errors.masv.errorMessage = 'Mã sinh viên phải gồm 10 ký tự số';
+    return false;
+  } else {
+    errors.masv.error = false;
+    errors.masv.errorMessage = '';
+  }
+  return true;
+};
+
+const handleSubmit = () => {
+  if (masvValidate()) {
+    alert('Mật khẩu mới đã được gửi vào email của bạn');
+  }
+};
 </script>
 
 <template>
-  <div class="login-cha">
-    <form class="login-con">
-      <img class="logo" style="width: 88px; height: 88px" src="../assets/logo.png" alt="logo" />
-      <div class="user-name">
-        <p>Tài khoản</p>
-        <div class="login">
-          <svg
-            class="ic"
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 56 56"
-          >
-            <path
-              fill="currentColor"
-              d="M28.012 27.367c5.039 0 9.375-4.5 9.375-10.36c0-5.788-4.36-10.077-9.375-10.077c-5.016 0-9.375 4.383-9.375 10.125c0 5.812 4.36 10.312 9.375 10.312M13.293 49.07h29.438c3.68 0 4.992-1.054 4.992-3.117c0-6.047-7.57-14.39-19.711-14.39c-12.164 0-19.735 8.343-19.735 14.39c0 2.063 1.313 3.117 5.016 3.117"
-            />
-          </svg>
-          <input
-            id="u-name"
-            type="text"
-            placeholder="Nhập tài khoản"
-            v-model="user.username"
-            required
-            autofocus
-          />
+    <div class="login-cha">
+      <form class="login-con" v-if="!forget" @submit.prevent="loginHandler">
+        <img class="logo" style="width: 88px; height: 88px" src="../assets/logo.png" alt="logo" />
+        <div class="user-name">
+          <p>Tài khoản</p>
+          <div class="login">
+            <svg class="ic" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 56 56">
+              <path fill="currentColor" d="M28.012 27.367c5.039 0 9.375-4.5 9.375-10.36c0-5.788-4.36-10.077-9.375-10.077c-5.016 0-9.375 4.383-9.375 10.125c0 5.812 4.36 10.312 9.375 10.312M13.293 49.07h29.438c3.68 0 4.992-1.054 4.992-3.117c0-6.047-7.57-14.39-19.711-14.39c-12.164 0-19.735 8.343-19.735 14.39c0 2.063 1.313 3.117 5.016 3.117"/>
+            </svg>
+            <input id="u-name" type="text" placeholder="Nhập tài khoản" v-model="user.username" required autofocus />
+          </div>
+          <p class="errorMsg" v-if="error.username.error">{{ error.username.errorMsg }}</p>
         </div>
-        <p class="errorMsg" v-if="error.username.error">{{ error.username.errorMsg }}</p>
-      </div>
-      <div class="password">
-        <p>Mật khẩu</p>
-        <div class="login">
-          <svg
-            class="ic"
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2m-6 9c-1.1 0-2-.9-2-2s.9-2 2-2s2 .9 2 2s-.9 2-2 2m3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1c1.71 0 3.1 1.39 3.1 3.1z"
-            />
-          </svg>
-          <input id="pw" type="password" placeholder="Nhập mật khẩu" v-model="user.password" />
+        <div class="password">
+          <p>Mật khẩu</p>
+          <div class="login">
+            <svg class="ic" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2m-6 9c-1.1 0-2-.9-2-2s.9-2 2-2s2 .9 2 2s-.9 2-2 2m3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1c1.71 0 3.1 1.39 3.1 3.1z"/>
+            </svg>
+            <input id="pw" type="password" placeholder="Nhập mật khẩu" v-model="user.password" />
+          </div>
+          <p class="errorMsg" v-if="error.password.error">{{ error.password.errorMsg }}</p>
         </div>
-        <p class="errorMsg" v-if="error.password.error">{{ error.password.errorMsg }}</p>
-      </div>
-      <a href="#">Quên mật khẩu? </a>
-      <button @click.prevent="loginHandler()">Đăng nhập</button>
-    </form>
-  </div>
-</template>
+        <p class="a" @click="forget = !forget">Quên mật khẩu?</p>
+        <button type="submit">Đăng nhập</button>
+      </form>
+      <form @submit.prevent="handleSubmit" v-else class="forgot-form">
+        <img class="logo" style="width: 88px; height: 88px" src="../assets/logo.png" alt="logo" />
 
+        <div>
+          <label for="masv"><p class="Masv">Mã sinh viên:</p></label>
+          <div class="login">
+            <svg class="ic" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 56 56">
+              <path fill="currentColor" d="M28.012 27.367c5.039 0 9.375-4.5 9.375-10.36c0-5.788-4.36-10.077-9.375-10.077c-5.016 0-9.375 4.383-9.375 10.125c0 5.812 4.36 10.312 9.375 10.312M13.293 49.07h29.438c3.68 0 4.992-1.054 4.992-3.117c0-6.047-7.57-14.39-19.711-14.39c-12.164 0-19.735 8.343-19.735 14.39c0 2.063 1.313 3.117 5.016 3.117"/>
+            </svg>
+            <input type="text" id="masv" v-model="formData.masv" placeholder="Nhập mã sinh viên" required />
+          </div>
+          <p class="errorMsg" v-if="errors.masv.error">{{ errors.masv.errorMessage }}</p>
+        </div>
+        <button type="submit">Gửi</button>
+        <p class="a" @click="forget = !forget">Quay lại</p>
+      </form>
+    </div>
+  </template>
 <style lang="scss" scoped>
 template {
   height: 100vh;
@@ -124,7 +148,7 @@ template {
 .login-cha {
   background-image: url(../assets/anhsinhnhat.png);
   width: 100%;
-  height: 100%;
+  height: 100vh;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
@@ -134,7 +158,8 @@ template {
   flex-direction: column;
   margin: 0;
 }
-.login-con {
+
+.login-con, .forgot-form {
   background-color: rgba(255, 255, 255, 0.4);
   -webkit-backdrop-filter: blur(5px);
   backdrop-filter: blur(5px);
@@ -149,40 +174,41 @@ template {
   border-radius: 25px;
   overflow: hidden;
   @include tablet {
-    width: 470px;
-    height: 490px;
+    max-width: 470px;
   }
   @include mobile {
-    width: 380px;
-    height: 480px;
-    
+    max-width: 380px;
   }
-  p.errorMsg {
-    color: rgb(238, 59, 59);
-    padding-bottom: 0px;
-    margin-top: 4px;
-  }
+
   .logo {
     margin-bottom: 50px;
+  }
+  .a {
+    padding-top: 20px;
+    margin-right: -39%;
+    color: #2a2a2a;
+    cursor: pointer;
   }
   .login {
     position: relative;
     font-size: 20px;
     color: #d9d9d9;
+    
+
     input {
-      padding-left: 30px;
-      width: 400px;
+        padding-left: 30px;
+        width: 400px;
       height: 35px;
       border-radius: 20px;
       border: none;
       @include tablet {
-        width: 380px;
-      }
-      @include mobile {
-        width: 250px;
         height: 32px;
       }
+      @include mobile {
+        height: 30px;
+      }
     }
+
     .ic {
       position: absolute;
       left: 8px;
@@ -193,34 +219,43 @@ template {
   .user-name {
     padding-bottom: 30px;
     p {
-      padding-bottom: 15px;
+      padding-bottom: 10px;
     }
   }
   .password {
     p {
-      padding-bottom: 15px;
+      padding-bottom: 10px;
     }
   }
-  a {
-    padding-top: 20px;
-    margin-right: -39%;
+
+  .a {
+    margin-top: 10px;
     color: #2a2a2a;
+    cursor: pointer;
   }
+
   button {
-    width: 440px;
+    width: 100%;
+    max-width: 440px;
     height: 35px;
     border-radius: 20px;
     background-color: #ff7e57;
     color: white;
-    margin-top: 70px;
+    margin-top: 20px;
     @include tablet {
-      margin-top: 40px;
-      width: 400px;
+      max-width: 400px;
     }
     @include mobile {
-      margin-top: 32px;
-      width: 280px;
+      max-width: 280px;
     }
   }
+
+  p.errorMsg {
+    color: rgb(238, 59, 59);
+    margin-top: 4px;
+  }
+ .Masv{
+    padding-bottom: 10px;
+ } 
 }
-</style>
+</style>  
