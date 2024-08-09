3e8f1ff047a5b55d6a882d8f1da3a031b806e5c9
Kang0408 <trandanhkhang482004@gmail.com>
2024-08-09 13:14:22 +0700

    fix: manage lesson

--- src/components/ClassManager/ClassManageRoadmap.vue
+++ src/components/ClassManager/ClassManageRoadmap.vue
@@ -16,34 +16,37 @@ const route = useRoute();
 const roadmapUrlFinal = ref('');
 
 function postRoadmap() {
-  dialog.warning({
-    title: 'Xác nhận',
-    content: `Bạn muốn gửi lộ trình này?`,
-    positiveText: 'Xác nhận',
-    negativeText: 'Hủy',
-    onPositiveClick: async () => {
-      const length = roadmapUrl.value.length;
-      for (let i = length; i >= 0; i--) {
-        if (roadmapUrl.value[i] === '/') {
-          roadmapUrlFinal.value = roadmapUrl.value.slice(0, i + 1) + 'preview';
-          break;
+  if (roadmapUrl.value) {
+    dialog.warning({
+      title: 'Xác nhận',
+      content: `Bạn muốn gửi lộ trình này?`,
+      positiveText: 'Xác nhận',
+      negativeText: 'Hủy',
+      onPositiveClick: async () => {
+        const length = roadmapUrl.value.length;
+        for (let i = length; i >= 0; i--) {
+          if (roadmapUrl.value[i] === '/') {
+            roadmapUrlFinal.value = roadmapUrl.value.slice(0, i + 1) + 'preview';
+            break;
+          }
         }
+        const res = await classStore.updateRoadmap(
+          { roadmap: roadmapUrlFinal.value },
+          route.params.id
+        );
+        if (res.status == 200) {
+          message.success('Gửi lộ trình thành công');
+          await classStore.getDetailClass(route.params.id);
+          roadmapUrl.value = '';
+        } else if (res.response.status) message.error('Gửi lộ trình thất bại');
+      },
+      onNegativeClick: () => {
+        message.error('Hủy');
       }
-      console.log(roadmapUrlFinal.value);
-      const res = await classStore.updateRoadmap(
-        { roadmap: roadmapUrlFinal.value },
-        route.params.id
-      );
-      if (res.status == 200) {
-        message.success('Gửi lộ trình thành công');
-        await classStore.getDetailClass(route.params.id);
-        roadmapUrl.value = '';
-      } else if (res.response.status) message.error('Gửi lộ trình thất bại');
-    },
-    onNegativeClick: () => {
-      message.error('Hủy');
-    }
-  });
+    });
+  } else {
+    message.info('Hãy gán link pdf');
+  }
 }
 
 onMounted(async () => {
@@ -53,7 +56,14 @@ onMounted(async () => {
 
 <template>
   <div class="class-roadmap-wrap">
-    <p class="heading">Lộ trình lớp học</p>
+    <p class="heading">
+      Lộ trình lớp học
+      <a
+        href="https://drive.google.com/file/d/18We9Lpk-Uiu4b98CzdoVSJbPFYzPTz4J/view?usp=sharing"
+        target="_blank"
+        >Hướng dẫn</a
+      >
+    </p>
     <div class="roadmap-input">
       <input type="text" placeholder="Đặt link tại đây" v-model="roadmapUrl" />
       <n-button type="primary" style="width: 64px" @click="postRoadmap">Gửi</n-button>
@@ -76,6 +86,12 @@ onMounted(async () => {
       font-size: 28px;
       margin: 24px 0;
       margin-left: 24px;
+    }
+    > a {
+      margin-left: 4px;
+      font-size: 20px;
+      text-decoration: underline;
+      color: blue;
     }
   }
   .roadmap {
